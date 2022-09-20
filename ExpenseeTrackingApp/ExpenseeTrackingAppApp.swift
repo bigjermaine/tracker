3bd0d24c673f5793ececc6462748e72adeeb9f13
daniel jermaine <danieljermaine2@gmail.com>
2022-09-20 02:20:06 +0100

    Initial Commit

---
+++ ExpenseeTrackingApp/ExpenseeTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseeTrackingAppApp.swift
+//  ExpenseeTrackingApp
+//
+//  Created by Apple on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseeTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
